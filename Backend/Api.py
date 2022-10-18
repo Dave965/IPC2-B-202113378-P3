4e1440318e14ada1d60d568726b123878696763f
David Noriega <dabrahamnz02@gmail.com>
2022-10-18 11:16:11 -0600

    feat: endpoints get y post

--- Backend/Api.py
+++ Backend/Api.py
@@ -8,10 +8,106 @@ global_clientes = [Cliente("nit", "nombre", "usuario", "clave", "direccion", "em
 
 app = Flask(__name__)
 
-@app.route("/principal")
-def principal():
-    data = [x.convertir_json() for x in global_clientes]
-    return jsonify({"datos": data})
+@app.route("/consultar_datos" )
+def consultar_datos():
+    usuarios = [x.convertir_json() for x in global_clientes]
+    categorias = [x.convertir_json() for x in global_categorias]
+    recursos = [x.convertir_json() for x in global_recursos]
+    configs =[x.convertir_json() for x in global_configs]
+    return jsonify({"usuarios": usuarios,
+                    "categorias": categorias,
+                    "recursos": recursos,
+                    "configs": configs})
+
+@app.route("/crear_recurso", methods = ["POST"])
+def crear_recurso():
+    n_recurso = Recurso(request.json["id_recurso"], request.json["nombre"],
+                        request.json["abrev"], request.json["metrica"],
+                        request.json["tipo"], request.json["precio"])
+
+    global_recursos.append(n_recurso)
+
+    return jsonify({"mensaje": "se ha creado el recurso con exito"})
+
+@app.route("/crear_categoria", methods = ["POST"])
+def crear_categoria():
+    json_lista_configuraciones = request.json["lista_configuraciones"]
+    
+    lista_configuraciones = [x for x in global_configs if x.id_configuracion in json_lista_configuraciones]
+    
+    n_categoria = Categoria(request.json["id_categoria"], request.json["nombre"],
+                        request.json["desc"], request.json["carga"], lista_configuraciones)
+
+    global_categorias.append(n_categoria)
+
+    return jsonify({"mensaje": "se ha creado la categoria con exito"})
+
+@app.route("/crear_configuracion", methods = ["POST"])
+def crear_configuracion():
+    json_lista_recursos = request.json["lista_recursos"]
+    lista_recursos = []
+    
+    for recurso in lista_recursos:
+        id_recurso = recurso["id_recurso"]
+        cantidad = recurso["cantidad"]
+        lista_recursos.append(Recurso_conf(id_recurso, cantidad))
+        
+    
+    n_configuracion = Configuracion(request.json["id_configuracion"], request.json["nombre"],
+                        request.json["desc"], lista_recursos)
+
+    global_configs.append(n_configuracion)
+
+    return jsonify({"mensaje": "se ha creado la configuracion con exito"})
+
+@app.route("/crear_cliente", methods = ["POST"])
+def crear_cliente():
+    lista_instancias = []
+
+    n_cliente = Cliente(request.json["nit"], request.json["nombre"],
+                        request.json["usuario"], request.json["clave"],
+                        request.json["direccion"], request.json["email"],
+                        lista_instancias)
+    
+    global_clientes.append(n_cliente)
+
+    return jsonify({"mensaje": "se ha creado el usuario con exito"})
+
+@app.route("/crear_instancia", methods = ["POST"])
+def crear_instancia():
+    cliente = [x for x in global_clientes if x.nit == request.json["nit"]]
+    
+    n_instancia = Instancia(request.json["id_instancia"], request.json["id_config"],
+                          request.json["f_inicio"], request.json["estado"], None)
+    
+    cliente[0].lista_instancias.append(n_instancia)
+
+    return jsonify({"mensaje": "se ha creado la instancia para el usuario "+cliente[0].nombre+"con exito"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True, port = 3100)
+
+
+
+
--- Backend/Clases.py
+++ Backend/Clases.py
@@ -56,12 +56,39 @@ class Recurso:
         self.tipo = tipo
         self.precio = precio
 
+    def convertir_json(self):
+        json_final = {}
+        json_final["id_recurso"] = self.id_recurso
+        json_final["nombre"] = self.nombre
+        json_final["abrev"] = self.abrev
+        json_final["metrica"] = self.metrica
+        json_final["tipo"] = self.tipo
+        json_final["precio"] = self.precio
+
+        return json_final
+
 class Configuracion:
     def __init__(self, id_configuracion, nombre, desc, lista_recursos):
         self.id_configuracion = id_configuracion
         self.nombre = nombre
         self.desc = desc
         self.lista_recursos = lista_recursos
+
+    def convertir_json(self):
+        json_final = {}
+        json_final["id_configuracion"] = self.id_configuracion
+        json_final["nombre"] = self.nombre
+        json_final["desc"] = self.desc
+
+        lista_recursos = []
+        
+        for recurso in self.lista_recursos:
+            lista_recursos.append(recurso.convertir_json())
+
+        json_final["lista_recursos"] = lista_recursos
+
+        return json_final
+            
 
 class Categoria:
     def __init__(self, id_categoria, nombre, desc, carga, lista_configuraciones):
@@ -71,7 +98,30 @@ class Categoria:
         self.carga = carga
         self.lista_configuraciones = lista_configuraciones
 
+    def convertir_json(self):
+        json_final = {}
+        json_final["id_categoria"] = self.id_categoria
+        json_final["nombre"] = self.nombre
+        json_final["desc"] = self.desc
+        json_final["carga"] = self.carga
+
+        lista_configuraciones = []
+        
+        for configuracion in self.lista_configuraciones:
+            lista_configuraciones.append(configuracion.id_configuracion)
+
+        json_final["lista_configuraciones"] = lista_configuraciones
+
+        return json_final
+
 class Recurso_conf:
     def __init__(self, id_recurso, cantidad):
         self.id_recurso = id_recurso
         self.cantidad = cantidad
+
+    def convertir_json(self):
+        json_final = {}
+        json_final["id_recurso"] = self.id_recurso
+        json_final["cantidad"] = self.cantidad
+
+        return json_final
